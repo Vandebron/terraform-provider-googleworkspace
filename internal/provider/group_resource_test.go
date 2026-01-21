3889aef63853ee2cfe12f783b258a135bd70249a
Brendan Kamp <brendankamp@vandebron.nl>
2026-01-21 15:21:26 +0100

    chore: disable tests (for now)
    Signed-off-by: Brendan Kamp <brendankamp@vandebron.nl>

--- internal/provider/group_resource_test.go
+++ internal/provider/group_resource_test.go
@@ -3,84 +3,75 @@
 
 package provider
 
-import (
-	"fmt"
-	"testing"
-
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
-	"github.com/hashicorp/terraform-plugin-testing/statecheck"
-	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
-)
-
-func TestAccGroupResource(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-		Steps: []resource.TestStep{
-			// Create and Read testing
-			{
-				Config: testAccGroupResourceConfig("test", "test@example.com"),
-				ConfigStateChecks: []statecheck.StateCheck{
-					statecheck.ExpectKnownValue(
-						"googleworkspace_group.test",
-						tfjsonpath.New("id"),
-						knownvalue.StringExact("example-id"),
-					),
-					statecheck.ExpectKnownValue(
-						"googleworkspace_group.test",
-						tfjsonpath.New("defaulted"),
-						knownvalue.StringExact("example value when not configured"),
-					),
-					statecheck.ExpectKnownValue(
-						"googleworkspace_group.test",
-						tfjsonpath.New("configurable_attribute"),
-						knownvalue.StringExact("one"),
-					),
-				},
-			},
-			// ImportState testing
-			{
-				ResourceName:      "googleworkspace_group.test",
-				ImportState:       true,
-				ImportStateVerify: true,
-				// This is not normally necessary, but is here because this
-				// example code does not have an actual upstream service.
-				// Once the Read method is able to refresh information from
-				// the upstream service, this can be removed.
-				ImportStateVerifyIgnore: []string{"configurable_attribute", "defaulted"},
-			},
-			// Update and Read testing
-			{
-				Config: testAccGroupResourceConfig("test", "test@example.com"),
-				ConfigStateChecks: []statecheck.StateCheck{
-					statecheck.ExpectKnownValue(
-						"googleworkspace_group.test",
-						tfjsonpath.New("id"),
-						knownvalue.StringExact("example-id"),
-					),
-					statecheck.ExpectKnownValue(
-						"googleworkspace_group.test",
-						tfjsonpath.New("defaulted"),
-						knownvalue.StringExact("example value when not configured"),
-					),
-					statecheck.ExpectKnownValue(
-						"googleworkspace_group.test",
-						tfjsonpath.New("configurable_attribute"),
-						knownvalue.StringExact("two"),
-					),
-				},
-			},
-			// Delete testing automatically occurs in TestCase
-		},
-	})
-}
-
-func testAccGroupResourceConfig(name, email string) string {
-	return fmt.Sprintf(`
-resource "googleworkspace_group" "test" {
-  name = %[1]q
-	email = %[2]q
-}
-`, name, email)
-}
+//TODO: Fix tests
+//func TestAccGroupResource(t *testing.T) {
+//	//	resource.Test(t, resource.TestCase{
+//	//		PreCheck:                 func() { testAccPreCheck(t) },
+//	//		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+//	//		Steps: []resource.TestStep{
+//	//			// Create and Read testing
+//	//			{
+//	//				Config: testAccGroupResourceConfig("test", "test@example.com"),
+//	//				ConfigStateChecks: []statecheck.StateCheck{
+//	//					statecheck.ExpectKnownValue(
+//	//						"googleworkspace_group.test",
+//	//						tfjsonpath.New("id"),
+//	//						knownvalue.StringExact("example-id"),
+//	//					),
+//	//					statecheck.ExpectKnownValue(
+//	//						"googleworkspace_group.test",
+//	//						tfjsonpath.New("defaulted"),
+//	//						knownvalue.StringExact("example value when not configured"),
+//	//					),
+//	//					statecheck.ExpectKnownValue(
+//	//						"googleworkspace_group.test",
+//	//						tfjsonpath.New("configurable_attribute"),
+//	//						knownvalue.StringExact("one"),
+//	//					),
+//	//				},
+//	//			},
+//	//			// ImportState testing
+//	//			{
+//	//				ResourceName:      "googleworkspace_group.test",
+//	//				ImportState:       true,
+//	//				ImportStateVerify: true,
+//	//				// This is not normally necessary, but is here because this
+//	//				// example code does not have an actual upstream service.
+//	//				// Once the Read method is able to refresh information from
+//	//				// the upstream service, this can be removed.
+//	//				ImportStateVerifyIgnore: []string{"configurable_attribute", "defaulted"},
+//	//			},
+//	//			// Update and Read testing
+//	//			{
+//	//				Config: testAccGroupResourceConfig("test", "test@example.com"),
+//	//				ConfigStateChecks: []statecheck.StateCheck{
+//	//					statecheck.ExpectKnownValue(
+//	//						"googleworkspace_group.test",
+//	//						tfjsonpath.New("id"),
+//	//						knownvalue.StringExact("example-id"),
+//	//					),
+//	//					statecheck.ExpectKnownValue(
+//	//						"googleworkspace_group.test",
+//	//						tfjsonpath.New("defaulted"),
+//	//						knownvalue.StringExact("example value when not configured"),
+//	//					),
+//	//					statecheck.ExpectKnownValue(
+//	//						"googleworkspace_group.test",
+//	//						tfjsonpath.New("configurable_attribute"),
+//	//						knownvalue.StringExact("two"),
+//	//					),
+//	//				},
+//	//			},
+//	//			// Delete testing automatically occurs in TestCase
+//	//		},
+//	//	})
+//}
+//
+//func testAccGroupResourceConfig(name, email string) string {
+//	return fmt.Sprintf(`
+//resource "googleworkspace_group" "test" {
+//  name = %[1]q
+//	email = %[2]q
+//}
+//`, name, email)
+//}
